583be22bab956905ca063064a2ada6fcb8a560e5
= <=duongquocanh368@gmail.com>
2023-08-19 16:16:05 +0700

    add test web

--- view.py
+++ view.py
@@ -2,8 +2,12 @@ from flask import Blueprint, render_template
 
 view = Blueprint("view","app")
 
-@view.route("/home")
-def homes():
-    return render_template("index.html",name="gay")
-
 @view.route("/")
+def homes():
+    return render_template("lol.html",name="gay")
+
+
+
+@view.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html",name=username)
